6123223f3070d3f0f6cdabb759b4ed74ef3659c9
ADR-43 <aarondaelramsey@gmail.com>
2025-10-25 15:18:55 -0500

    Chore: Updated script and removed erroneously named .rda file.

--- data-raw/prepare_kb_data.R
+++ data-raw/prepare_kb_data.R
@@ -11,11 +11,18 @@ library(usethis)    # for use_data()
 kb_raw <- read_csv("data-raw/kb_covars.csv")
 
 # ---- Clean / Transform ----
-kb_data <- kb_raw %>%
+kb_covars <- kb_raw %>%
   rename_with(tolower)
 
+# ---- Print Columns and Data Types ----
+message("--- Column Names (kb_data) ---")
+print(names(kb_covars))
+message("\n--- Data Types (kb_data) ---")
+print(str(kb_covars))
+message("---------------------------------\n")
+
 # ---- Verify ----
-stopifnot(!any(is.na(kb_data)))
+stopifnot(!any(is.na(kb_covars)))
 
 # ---- Save ----
-usethis::use_data(kb_data, overwrite = TRUE)
+usethis::use_data(kb_covars, overwrite = TRUE)
